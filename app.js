b05ddd20d8ba254200afa628ea275720655ec6cb
Ellena Tzavelas <wakandacat@gmail.com>
2024-03-03 17:54:56 -0500

    start UI selection mostly working

--- app.js
+++ app.js
@@ -14,15 +14,6 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-//create our routes
-app.get('/player1', function(req, res) {
-    res.sendFile(__dirname + '/public/player1.html');
-});
-
-//create our routes
-app.get('/player2', function(req, res) {
-    res.sendFile(__dirname + '/public/player2.html');
-});
 
 //websocket stuff
 //.on is an event listener
@@ -30,27 +21,21 @@ app.get('/player2', function(req, res) {
 io.on('connection', (socket) => { //listening for all socket connection events
     console.log(socket.id + " is connected.");
 
-//     var clients_in_the_room = io.sockets.adapter.rooms[roomId];
-//     for (var clientId in clients_in_the_room ) {
-//     console.log('client: %s', clientId); // Seeing is believing
-//    // var client_socket = io.sockets.connected[clientId]; // Do whatever you want with this
-//     }
-
     //listen to this specific socket event
     socket.on('disconnect', () => {
         console.log(socket.id + " is disconnected.");
     });
 
-    // //custom events
-    // socket.on('red', (data) => {
-    //     console.log("red event received.");
-    //     io.emit('color_change', {r:255, g:0, b:0}); //send an event to all clients
-    // });
+    //custom events
+    socket.on('red', (data) => {
+        console.log("red event received from" + data);
+        io.emit('circleChoice', 'red' + data); //send an event to all clients
+    });
 
-    // socket.on('blue', (data) => {
-    //     console.log("blue event received.");
-    //     io.emit('color_change', {r:0, g:0, b:255}); //send an event to all clients
-    // });
+    socket.on('blue', (data) => {
+        console.log("blue event received from" + data);
+        io.emit('circleChoice', 'blue' + data); //send an event to all clients
+    });
 });
 
 //start our server
